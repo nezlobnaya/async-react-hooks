21070bd3259e4ee94304cbacc9e9f84e08f0eb94
Vladislav Burlutskiy <vlad.burlutskiy@icloud.com>
2019-10-31 11:19:15 -0400

    First commit

---
+++ src/components/AsyncHooks/AsyncHooks.js
@@ -0,0 +1,16 @@
+import React, { useState } from 'react'
+
+export default function AsyncHooks() {
+    const[search, setSearch] = useState('')
+    return (
+        <div>
+            <h1>Async React Hooks</h1>
+            <form>
+                <input
+                    value={search}
+                    onChange={e => setSearch(e.target.value)}
+                    placeholder='Search for Gifs!' />
+            </form>
+        </div>
+    )
+}
